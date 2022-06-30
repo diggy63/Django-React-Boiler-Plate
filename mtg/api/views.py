ad04b5d40d5b824e1c58eee1c7175b34e1a5b776
diggy63 <daniel.kanczugowski@gmail.com>
2022-06-29 17:59:08 -0700

    first commit

---
+++ mtg/api/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
+
+# Create your views here.
+
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+class CreateRoomView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
